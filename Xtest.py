184b49b8d82942b18f373ea152ef6a843c6d0d71
ksharp1990song <fankai61@gmail.com>
2023-05-12 18:54:26 +0200

    12.05

--- Xtest.py
+++ Xtest.py
@@ -1,1 +1,12 @@
-int_list = [1,2,3,4,5]
+import pandas as pd
+
+data = {'current_time': [], 'signal_state1': [], 'gs': [], 'rs': []}
+
+with open('D:/Simulationsmodell HERCULES/V0/test1her.txt', 'r') as f:
+    for line in f:
+        key, value = line.strip('\n').split(':')
+        data[key].append(int(value))
+        
+df = pd.DataFrame(data)
+df.drop_duplicates(subset=['current_time'], keep='first', inplace=True)
+df.to_excel('D:/Simulationsmodell HERCULES/V0/test2her.xlsx', index=False)
