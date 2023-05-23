8d9f7bfe289e681fb277cc0b70be15c0fd00fcc8
ksharp1990song <fankai61@gmail.com>
2023-05-23 14:55:00 +0200

    19.05

--- Xtest.py
+++ Xtest.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = {'current_time': [], 'signal_state1_szp': [], 'signal_state2_szp': [], 'signal_state3_szp': [], 'signal_state4_szp': [], 'signal_state5_szp': [], 'signal_state6_szp': [], 'gs1': [], 'rs1': [],  'gs2': [], 'rs2': [],  'gs3': [], 'rs3': [],  'gs4': [], 'rs4': [],  'gs5': [], 'rs5': [],  'gs6': [], 'rs6': [], 'predict_gs': [], 'predict_rs': []}
+data = {'current_time': [], 'real_time': [], 'signal_state1_szp': [], 'signal_state2_szp': [], 'signal_state3_szp': [], 'signal_state4_szp': [], 'signal_state5_szp': [], 'signal_state6_szp': [], 'gs1': [], 'rs1': [],  'gs2': [], 'rs2': [],  'gs3': [], 'rs3': [],  'gs4': [], 'rs4': [],  'gs5': [], 'rs5': [],  'gs6': [], 'rs6': [], 'predict_ng': [], 'predict_nr': []}
 with open('D:/Simulationsmodell HERCULES/V0/test1her.txt', 'r') as f:
     for line in f:
         key, value = line.strip('\n').split(':')
@@ -8,4 +8,4 @@ with open('D:/Simulationsmodell HERCULES/V0/test1her.txt', 'r') as f:
         
 df = pd.DataFrame(data)
 df.drop_duplicates(subset=['current_time'], keep='first', inplace=True)
-df.to_excel('D:/Simulationsmodell HERCULES/V0/test4her.xlsx', index=False)
+df.to_excel('D:/Simulationsmodell HERCULES/V0/test5her.xlsx', index=False)
